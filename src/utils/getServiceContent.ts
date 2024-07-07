662addf4fb4c7457713278ae6911394509eb8af2
xXAvoraXx <burakgunduz_@outlook.com>
2024-07-07 18:43:15 +0300

    1.0.9

--- src/utils/getServiceContent.ts
+++ src/utils/getServiceContent.ts
@@ -1,9 +1,9 @@
 export default (apiPath: string, requestLibPath: string, responseType: string) => {
   return `\
 export async function getRoutersInfo() {
-  const requestLib = require(${requestLibPath});
+  const requestLib = require(\`${requestLibPath}\`);
   try {
-    const response = await requestLib.get(${apiPath});
+    const response = await requestLib.get('${apiPath}');
     const routes = response.${responseType};
     return routes;
   } catch (error) {
